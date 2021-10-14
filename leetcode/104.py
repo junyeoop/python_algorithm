a5ddee0f97324a175b4c0447d44b41258ab3ad99
JUNYEOP <55452516+junyeoop@users.noreply.github.com>
2021-10-14 21:38:26 +0900

    Create 104.py

---
+++ leetcode/104.py
@@ -0,0 +1,31 @@
+# maximum-depth-of-binary-tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+import collections
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        queue = collections.deque([root])
+        depth = 0
+        
+        while(queue):
+            depth += 1
+            
+            for _ in range(len(queue)):
+                cur_root = queue.popleft()
+                
+                if cur_root.left:
+                    queue.append(cur_root.left)
+                if cur_root.right:
+                    queue.append(cur_root.right)
+        return depth
+        
+        
