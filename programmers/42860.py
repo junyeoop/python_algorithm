315d0252a0b2273406f7ad6a1bd0e588d4a6bcb6
JUNYEOP <junyeoop@gmail.com>
2021-11-04 01:02:38 +0900

    Create 42860.py

---
+++ programmers/42860.py
@@ -0,0 +1,8 @@
+def solution(name):
+    answer = 0
+    finished = [0 for _ in range(len(name))]
+    alpha = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, X, Y, Z]
+    ud_count = 0
+    lr_count = 0
+
+    return answer
